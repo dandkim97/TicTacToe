f306ad99aae9af79e9b001644e05413ecdb39583
Dan Kim <dan.dkyu.kim@gmail.com>
2019-08-30 20:30:04 -0400

    added opposite corner, center, half of blockFork cases with testing

--- src/Main/Bot.java
+++ src/Main/Bot.java
@@ -14,12 +14,13 @@ public class Bot {
 //
 //        } else if (canBlockFork()){
 //
-//        } else if (canSetCenter()) {
-//
-//        if (canSetOppositeCorner()) {
-//
-//        }
-        if (canSetEmptyCorner(board)) {
+        if (canSetCenter(board)) {
+            newBoard = setCenter(board);
+        }
+        else if (canSetOppositeCorner(board)) {
+            newBoard = setOppositeCorner(board);
+        }
+        else if (canSetEmptyCorner(board)) {
             newBoard = setEmptyCorner(board);
         }
         else if (canSetEmptySide(board)) {
@@ -28,10 +29,39 @@ public class Bot {
         return newBoard;
     }
 
+    // Add blockFork and testing
+    private boolean canBlockFork(char[][] board){
+        return( isX(board[0][0]) && isX(board[2][2])
+                || isX(board[0][2]) && isX(board[2][0]) );
+    }
 
+    private char[][] setCenter(char[][] board){
+        board[1][1] = 'O';
+        return board;
+    }
 
+    private boolean canSetCenter(char[][] board){
+        return (isEmpty(board[1][1]));
+    }
 
+    private char[][] setOppositeCorner(char[][] board){
+        if(!isEmpty(board[0][0]) && isEmpty(board[2][2]))
+            board[2][2] = 'O';
+        else if(!isEmpty(board[0][2]) && isEmpty(board[2][0]))
+            board[2][0] = 'O';
+        else if(!isEmpty(board[2][2]) && isEmpty(board[0][0]))
+            board[0][0] = 'O';
+        else if(!isEmpty(board[2][0]) && isEmpty(board[0][2]))
+            board[0][2] = 'O';
+        return board;
+    }
 
+    private boolean canSetOppositeCorner(char[][] board){
+        return (!isEmpty(board[0][0]) && isEmpty(board[2][2])
+                || !isEmpty(board[0][2]) && isEmpty(board[2][0])
+                || !isEmpty(board[2][2]) && isEmpty(board[0][0])
+                || !isEmpty(board[2][0]) && isEmpty(board[0][2]) );
+    }
 
     private char[][] setEmptyCorner(char[][] board){
         if(isEmpty(board[0][0]) ){
@@ -75,6 +105,10 @@ public class Bot {
 
     private boolean isEmpty(char c){
         return (c == 0);
+    }
+
+    private boolean isX(char c){
+        return (c == 'X');
     }
 }
 
--- src/Testing/BotTest.java
+++ src/Testing/BotTest.java
@@ -9,12 +9,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class BotTest {
 
     @Test
+    void testIfBotPlacesOnCenter(){
+        Bot testBot = new Bot();
+        char[][] centerExpected = new char[3][3];
+        centerExpected[1][1] = 'O';
+        GameBoard testBoard = new GameBoard();
+        testBoard.add(0,0);
+        char[][] test = testBot.addPhase(testBoard);
+        assertEquals(test[1][1], centerExpected[1][1]);
+    }
+
+    @Test
+    void testIfBotPlacesOnOppositeCorner(){
+        Bot testBot = new Bot();
+        char[][] botRightExpected = new char[3][3];
+        botRightExpected[2][2] = 'O';
+        GameBoard testBoard = new GameBoard();
+        testBoard.add(0,0);
+        char[][] test = testBot.addPhase(testBoard);
+        assertEquals(test[2][2], botRightExpected[2][2]);
+    }
+
+    @Test
     void testIfBotPlacesOnEmptyCorner(){
         Bot testBot = new Bot();
-        char[][] topLeftMarked = new char[3][3];
-        topLeftMarked[0][0] = 'O';
+        char[][] topLeftExpected = new char[3][3];
+        topLeftExpected[0][0] = 'O';
         GameBoard testBoard = new GameBoard();
         char[][] test = testBot.addPhase(testBoard);
-        assertEquals(test[0][0], topLeftMarked[0][0]);
+        assertEquals(test[0][0], topLeftExpected[0][0]);
     }
 }
